98c257c4f9b540538bb5ddb389fd898b31cf819c
Marina Bokach <marina.bokach@admiralmarkets.com>
2022-03-11 11:37:28 +0300

    homework: task-15 - add api

--- js-task_15_api/js/script.js
+++ js-task_15_api/js/script.js
@@ -2,11 +2,7 @@ class User {
  constructor(data) {
   this.user = {};
   if (data) {
-   this.user.id = data.id;
-   this.user.name = data.name;
-   this.user.email = data.email;
-   this.user.address = data.address;
-   this.user.phone = data.phone;
+   this.user = data;
   }
  }
 
@@ -31,6 +27,7 @@ class Contacts {
    let user = new User(data);
    user.edit({id: userId});
    this.contacts.push(user);
+   this.storage = this.contacts;
   }
  }
 
@@ -72,62 +69,13 @@ class ContactsApp extends Contacts {
  constructor() {
   super();
   this.init();
-  this.contactInfo();
+  // this.contactInfo();
  }
 
- init() {
-  let div = this.createField('div', new Map([
-   ['class', 'container']
-  ]));
-  let form = this.createField('form', new Map([
-   ['id', 'form'],
-   ['class', 'form']
-  ]));
-
-  let name = this.createField('input', new Map([
-   ['name', 'name'],
-   ['type', 'text'],
-   ['placeholder', 'Name']
-  ]));
-
-  let email = this.createField('input', new Map([
-   ['name', 'email'],
-   ['type', 'email'],
-   ['placeholder', 'email@email.com']
-  ]));
-
-  let address = this.createField('input', new Map([
-   ['name', 'address'],
-   ['type', 'text'],
-   ['placeholder', 'Address']
-  ]));
-
-  let phone = this.createField('input', new Map([
-   ['name', 'phone'],
-   ['type', 'tel'],
-   ['placeholder', 'Phone']
-  ]));
-
-  let submitBtn = this.createField('input', new Map([
-   ['type', 'submit'],
-   ['class', 'button']
-  ]));
-  submitBtn.value = 'Add contact';
-
-  let contactsList = document.createElement('ul');
-  contactsList.setAttribute('class', 'contacts_list');
-  this.contactsList = contactsList;
-
-  form.append(name, email, address, phone, submitBtn);
-  document.body.append(div);
-  div.append(form, contactsList);
-
-  this.inputs = [name, email, address, phone];
-
-  form.addEventListener('submit', (event) => {
-   this.addContact(event)
-  });
-
+ async init() {
+  this.renderForm();
+  await this.getData();
+  this.contactInfo();
  }
 
  addContact(event) {
@@ -135,12 +83,9 @@ class ContactsApp extends Contacts {
   let data = this.inputs.reduce((obj, elem) => ({...obj, [elem.name]: elem.value}), {});
 
   this.add(data);
-  this.storage = this.contacts;
-  // this.setStorage();
   this.inputs.forEach(elem => elem.value = '');
 
   this.contactInfo();
-
  }
 
  contactInfo() {
@@ -212,13 +157,11 @@ class ContactsApp extends Contacts {
   });
 
   this.edit(id, data);
-  // this.setStorage();
   this.storage = this.contacts;
  }
 
  removeContact(id) {
   this.remove(id);
-  // this.setStorage();
   this.storage = this.contacts;
   this.contactInfo();
  }
@@ -240,31 +183,68 @@ class ContactsApp extends Contacts {
   return field;
  }
 
- // getStorage() {
- //  let localContactData = localStorage.getItem('contacts');
- //  if (localContactData && localContactData.length > 0) {
- //   return JSON.parse(localContactData);
- //  }
- //
- //  return {};
- // }
+ renderForm() {
+  let div = this.createField('div', new Map([
+   ['class', 'container']
+  ]));
+  let form = this.createField('form', new Map([
+   ['id', 'form'],
+   ['class', 'form']
+  ]));
 
- // setStorage() {
- //  localStorage.setItem('contacts', JSON.stringify(this.contacts));
- //  document.cookie = 'storageExpiration=1; max-age: 864000';
- //
- //  if (!getCookie('storageExpiration')) {
- //   localStorage.removeItem('contacts');
- //  }
- // }
+  let name = this.createField('input', new Map([
+   ['name', 'name'],
+   ['type', 'text'],
+   ['placeholder', 'Name']
+  ]));
+
+  let email = this.createField('input', new Map([
+   ['name', 'email'],
+   ['type', 'email'],
+   ['placeholder', 'email@email.com']
+  ]));
+
+  let address = this.createField('input', new Map([
+   ['name', 'address'],
+   ['type', 'text'],
+   ['placeholder', 'Address']
+  ]));
+
+  let phone = this.createField('input', new Map([
+   ['name', 'phone'],
+   ['type', 'tel'],
+   ['placeholder', 'Phone']
+  ]));
+
+  let submitBtn = this.createField('input', new Map([
+   ['type', 'submit'],
+   ['class', 'button']
+  ]));
+  submitBtn.value = 'Add contact';
+
+  let contactsList = document.createElement('ul');
+  contactsList.setAttribute('class', 'contacts_list');
+  this.contactsList = contactsList;
+
+  form.append(name, email, address, phone, submitBtn);
+  document.body.append(div);
+  div.append(form, contactsList);
+
+  this.inputs = [name, email, address, phone];
+
+  form.addEventListener('submit', (event) => {
+   this.addContact(event)
+  });
+
+ }
 
  set storage(data) {
   localStorage.setItem('contacts', JSON.stringify(this.contacts));
   document.cookie = 'storageExpiration=1; max-age: 864000';
 
-  if (!getCookie('storageExpiration') && localStorage.getItem('contacts')) {
-   localStorage.removeItem('contacts');
-  }
+  // if (!getCookie('storageExpiration') && localStorage.getItem('contacts')) {
+  //  localStorage.removeItem('contacts');
+  // }
  }
 
  get storage() {
@@ -274,6 +254,20 @@ class ContactsApp extends Contacts {
   }
 
   return {};
+ }
+
+ async getData() {
+
+  console.log("getData");
+  let url = 'https://jsonplaceholder.typicode.com/users';
+
+  await fetch(url)
+  .then(response => response.json())
+  .then(data => {
+   data.forEach(item => {
+    this.add(item);
+   });
+  });
  }
 }
 
